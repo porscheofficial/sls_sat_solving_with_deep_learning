66b48ced08e3f0ac51fb9bab298cd3ae20614e35
maximilian.kramer <maximilian.kramer@porsche.digital>
2023-05-24 15:54:03 +0200

    added experiments and fixed a bug with candidates

--- python/src/data_utils.py
+++ python/src/data_utils.py
@@ -95,9 +95,13 @@ class SATTrainingDataset(data.Dataset):
             target_name = instance_name + "_sol.pkl"
             with open(target_name, "rb") as f:
                 solution_dict = pickle.load(f)
-                candidates = np.array(list(solution_dict.values()), dtype=int).reshape(
-                    1, -1
-                )  # (1, n_node)
+                if type(solution_dict) == dict:
+                    candidates = np.array(list(solution_dict.values()), dtype=int).reshape(
+                                                1, -1
+                                            )  # (1, n_node)
+                else:
+                    candidates = np.array([solution_dict])
+
 
         padded_candidates = self.representation.get_padded_candidate(
             candidates, self.max_n_node
