eb5b1f39b02aa4eedc6e427fad6dcd3ad64713df
maximilian.kramer <maximilian.kramer@porsche.digital>
2023-06-02 22:10:23 +0200

    added experiments and evaluation

--- experiments/run_3SAT_aws_right_layer_number.sh
+++ experiments/run_3SAT_aws_right_layer_number.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for config in experiments/configs_aws/random_3_SAT/right_inv_temp/*.yaml; do
+for config in experiments/configs_aws/random_3_SAT/right_layer_number/*.yaml; do
     python -m python.src.train --config $config
 done
