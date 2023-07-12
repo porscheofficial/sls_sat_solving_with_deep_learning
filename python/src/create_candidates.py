6f1c3a8b70b9b2e4c9416eef98fc55d90dfd2b4a
maximilian.kramer <maximilian.kramer@porsche.digital>
2023-07-12 11:34:35 +0200

    added feature to sample candidates on aws

---
+++ python/src/create_candidates.py
@@ -0,0 +1,5 @@
+from generate_random_instances import create_candidates_with_sol
+path = "../../../data_random_3SAT_NEW/"
+sample_size = 100
+threshold = 0.05
+create_candidates_with_sol(path, sample_size, threshold)
