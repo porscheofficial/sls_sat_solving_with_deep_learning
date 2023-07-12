fa8c2f03651ccae1f601a6a83d494bb28d5937f5
maximilian.kramer <maximilian.kramer@porsche.digital>
2023-07-12 13:11:30 +0200

    added experiments

--- experiments/run_3SAT_aws_NEW.sh
+++ experiments/run_3SAT_aws_NEW.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-for config in experiments/configs_aws/random_3_SAT_NEW/*.yaml; do
+for config in experiments/configs_aws/random_3SAT_NEW/*.yaml; do
     python -m python.src.train --config $config
 done
