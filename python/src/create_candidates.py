de3dd7419075576571702dce8f66016404ab217d
Paul Boes <paul.boes@porsche.digital>
2023-08-17 16:31:57 +0200

    deleting more unused files

--- python/src/create_candidates.py
+++
@@ -1,8 +0,0 @@
-import glob
-from generate_random_instances import create_candidates_with_sol
-
-path = "../../../data_random_3SAT_NEW/"
-print(glob.glob(path + "*"))
-sample_size = 100
-threshold = 0.05
-create_candidates_with_sol(path, sample_size, threshold)
