78056bb3e7eadd37bb85127d5a9416a4ddd6fd67
maximilian.kramer <maximilian.kramer@porsche.digital>
2023-09-05 13:55:19 +0200

    linting

--- python/src/generate_random_instances.py
+++ python/src/generate_random_instances.py
@@ -35,24 +35,28 @@ def create_candidates_with_sol(data_dir, sample_size, threshold):
         threshold (float): float ranging from 0 to 1. This is the probability that a spin flip is executed on a variable in the solution string
     """
     solved_instances = glob.glob(join(data_dir, "*_sol.pkl"))
-    for g in solved_instances:
-        with open(g, "rb") as f:
-            p = pickle.load(f)
-        if type(p) == dict:
-            print("dict", p)
-            p = [x for (_, x) in p.items()]
-        if type(p) == list or type(p) == np.array:
-            if 2 in p or -2 in p:
-                p = np.array(p, dtype=float)
-                p = [int(np.sign(x) + 1) / 2 for x in p]
-        print(p)
-        n = np.array(p, dtype=bool)
-        print(n)
-        samples = sample_candidates(n, sample_size - 1, threshold)
-        samples = np.concatenate((np.reshape(n, (1, len(n))), samples), axis=0)
-        name = g.split("_sol.pkl")[0]
-        with open(name + "_samples_sol.npy", "wb") as f:
-            np.save(f, samples)
+    for solved_instance in solved_instances:
+        with open(solved_instance, "rb") as file:
+            solution = pickle.load(file)
+        if type(solution) == dict:
+            print("dict", solution)
+            solution = [assignment_x for (_, assignment_x) in solution.items()]
+        if type(solution) == list or type(solution) == np.array:
+            if 2 in solution or -2 in solution:
+                solution = np.array(solution, dtype=float)
+                solution = [
+                    int(np.sign(assignment_x) + 1) / 2 for assignment_x in solution
+                ]
+        print(solution)
+        solution_boolean = np.array(solution, dtype=bool)
+        print(solution_boolean)
+        samples = sample_candidates(solution_boolean, sample_size - 1, threshold)
+        samples = np.concatenate(
+            (np.reshape(solution_boolean, (1, len(solution_boolean))), samples), axis=0
+        )
+        name = solved_instance.split("_sol.pkl")[0]
+        with open(name + "_samples_sol.npy", "wb") as file:
+            np.save(file, samples)
 
 
 def sample_candidates(original, sample_size, threshold):
@@ -71,63 +75,70 @@ def sample_candidates(original, sample_size, threshold):
     return np.where(condition, np.invert(original), original)
 
 
-def generate_random_KCNF(k, n, m, path, TIMEOUT=100):
+def generate_random_KCNF(k_locality, n_variables, n_clauses, path, timeout=100):
     """Generate a single random_KCNF formula.
 
     Args:
-        k (int): locality of instance
-        n (int): number of variables of instance
-        m (int): number of clauses contained in instance
+        k_locality (int): locality of instance
+        n_variables (int): number of variables of instance
+        n_clauses (int): number of clauses contained in instance
         path (str): path and name how instance should be saved
-        TIMEOUT (int, optional): how often we try to generate a satisfying formula until we return no instance. Defaults to 100.
+        timeout (int, optional): how often we try to generate a satisfying formula until we return no instance. Defaults to 100.
     """
-    t = 0
+    current_time = 0
     sol = False
-    while t <= TIMEOUT and sol == False:
-        t += 1
+    while current_time <= timeout and sol == False:
+        current_time += 1
         # print(t)
-        cnf = cnfgen.RandomKCNF(k, n, m)
+        cnf = cnfgen.RandomKCNF(k_locality, n_variables, n_clauses)
         cnf = cnf.to_dimacs()
         cnf = CNF(from_string=cnf)
-        g = Glucose3(cnf)
-        if g.solve() == True:
-            sol = g.get_model()
+        solver_result = Glucose3(cnf)
+        if solver_result.solve() == True:
+            sol = solver_result.get_model()
             cnf.to_file(path + ".cnf")
-            with open(path + "_sol.pkl", "wb") as f:
-                pickle.dump(sol, f)
+            with open(path + "_sol.pkl", "wb") as file:
+                pickle.dump(sol, file)
     if sol == False:
         print(
             "no satisfiable random_KCNF problem found for (n,k,m)=("
-            + str(n)
+            + str(n_variables)
             + ","
-            + str(k)
+            + str(k_locality)
             + ","
-            + str(m)
+            + str(n_clauses)
             + ")"
         )
 
 
 def generate_dataset_random_KCNF(
-    k, n_list, alpha, num_samples, path, vary_percent=0, TIMEOUT=100
+    k_locality, n_variables_list, alpha, num_samples, path, vary_percent=0, TIMEOUT=100
 ):
     """Generate a random_KCNF dataset.
 
     Args:
-        n_list (list): list of number of variables that should be used for generating
+        k_locality (int): locality of clauses
+        n_variables_list (list): list of number of variables that should be used for generating
         alpha (float): float describing the desired density of KCNF
         num_samples (int): number of samples generated per value in n_list
         path (str): path of where dataset should be saved
         vary_percent (float, optional): describes by how much we vary m at maximum
         TIMEOUT (int, optional): how often we try to generate a satisfying formula until we return no instance. Defaults to 100.
     """
-    for n in n_list:
+    for n_variables in n_variables_list:
         for _ in range(num_samples):
             vary = 2 * (1 / 2 - random.random()) * vary_percent
-            m = int((1 + vary) * alpha * n)
+            n_clauses = int((1 + vary) * alpha * n_variables)
             index = str(random.randint(0, 10000000))
-            params = str(k) + "_" + str(n) + "_" + str(m) + "_"
+            params = (
+                str(k_locality) + "_" + str(n_variables) + "_" + str(n_clauses) + "_"
+            )
             generate_random_KCNF(
-                k, n, m, path=path + "random_KCNF" + params + index, TIMEOUT=TIMEOUT
+                k_locality,
+                n_variables,
+                m,
+                path=path + "random_KCNF" + params + index,
+                timeout=TIMEOUT,
             )
 
 
