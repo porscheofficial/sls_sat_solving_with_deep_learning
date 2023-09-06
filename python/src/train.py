60a30bc05d7f30c7ba4607280c48af510dac23ec
maximilian.kramer <maximilian.kramer@porsche.digital>
2023-09-06 09:59:52 +0200

    linting

--- python/src/train.py
+++ python/src/train.py
@@ -17,19 +17,18 @@ from torch.utils import data
 import matplotlib.pyplot as plt
 from jsonargparse import CLI
 
-
-sys.path.append("../../")
-
 from python.src.data_utils import SATTrainingDataset, JraphDataLoader
-from python.src.sat_representations import VCG, LCG, SATRepresentation
+from python.src.sat_representations import VCG, LCG
 from python.src.model import get_network_definition
 from python.src.train_utils import (
     plot_accuracy_fig,
-    initiate_eval_objects_loss,
     update_eval_objects_loss,
-    initiate_eval_moser_loss,
     update_eval_moser_loss,
+    initiate_eval_objetcts_train_test,
+    initiate_eval_moser_train_test,
 )
+
+sys.path.append("../../")
 
 
 def train(
@@ -38,9 +37,9 @@ def train(
     alpha: float,
     beta: float,
     gamma: float,
-    NUM_EPOCHS: int,
-    N_STEPS_MOSER: int,
-    N_RUNS_MOSER: int,
+    num_epochs: int,
+    n_steps_moser: int,
+    n_runs_moser: int,
     data_path,
     graph_representation,
     network_type,
@@ -60,19 +59,19 @@ def train(
         alpha (float): prefactor used for Gibbs loss
         beta (float): prefactor used for LLL loss
         gamma (float): prefactor used for alternative LLL loss
-        NUM_EPOCHS (int): _description_
-        N_STEPS_MOSER (int): number of steps used in MT algorithm statistics
-        N_RUNS_MOSER (int): number of runs used in MT algorithm statistics
+        num_epochs (int): _description_
+        n_steps_moser (int): number of steps used in MT algorithm statistics
+        n_runs_moser (int): number of runs used in MT algorithm statistics
         path (str): path pointing to training dataset (this is split into train and test set)
-        graph_representation (SATRepresentation): SATRepresentation used. Either LCG or VCG. Note: here you have to use the SATRepresentation!!!
-        network_type (str): either "interaction" for interaction network or "GCN" for Graph convolutional network (not tested!)
+        graph_representation (SATRepresentation): SATRepresentation used. Either LCG or VCG.
+        network_type (str): either "interaction" or "GCN".
         mlp_layers (array): size of mlp_layers. For example: [200,200]
-        img_path (bool, optional): path where the plot is saved that contains the loss function plot as a function of the epochs. Defaults to False.
+        img_path (bool, optional): path where the plot is saved. Defaults to False.
         model_path (bool, optional): path where the model is saved. Defaults to False.
         experiment_tracking (bool, optional): decide whether experiment tracking is done using MLflow. Defaults to False.
         return_candidates (bool, optional): decide whether candidates are used for Gibbs loss or only the solution. Defaults to False.
         initial_learning_rate (float, optional): initial learning rate that is chosen. Defaults to 0.001.
-        final_learning_rate (float, optional): final learning rate that is chosen. Note that the learning rate decays from the initial learing rate exponentially to the final learning rate over the epochs. Defaults to 0.001.
+        final_learning_rate (float, optional):  final learning rate that is chosen. Note that the learning rate decays from the initial learing rate exponentially to the final learning rate over the epochs. Defaults to 0.001.
 
     Returns:
         @TODO: type: final params of the net
@@ -93,7 +92,6 @@ def train(
     train_eval_data, _ = data.random_split(
         train_data, [0.2, 0.8], generator=Generator().manual_seed(0)
     )
-    t1 = time.time()
     train_loader = JraphDataLoader(train_data, batch_size=batch_size, shuffle=True)
     test_loader = JraphDataLoader(test_data, batch_size=batch_size)
     train_eval_loader = JraphDataLoader(train_eval_data, batch_size=batch_size)
@@ -106,7 +104,7 @@ def train(
     params = network.init(jax.random.PRNGKey(42), sat_data[0][0].graph)  # type: ignore[attr-defined]
 
     # use a schedule function for the ADAM optimizer
-    tot_steps = int(NUM_EPOCHS * np.ceil(len(train_data) / batch_size))
+    tot_steps = int(num_epochs * np.ceil(len(train_data) / batch_size))
     decay_rate = final_learning_rate / initial_learning_rate
     exponential_decay_scheduler = optax.exponential_decay(
         init_value=initial_learning_rate,
@@ -115,12 +113,7 @@ def train(
         transition_begin=int(tot_steps * 0.05),
         staircase=False,
     )
-    print(
-        "initial decay rate:",
-        initial_learning_rate,
-        "final learning rate:",
-        final_learning_rate,
-    )
+
     opt_init, opt_update = optax.adam(learning_rate=exponential_decay_scheduler)
     opt_state = opt_init(params)
 
@@ -168,38 +161,37 @@ def train(
 
     @jax.jit
     def update(params, batch, opt_state):
-        g = jax.grad(total_loss)(
+        gradient = jax.grad(total_loss)(
             params, batch, inv_temp, alpha, beta, gamma, graph_representation
         )
-        updates, opt_state = opt_update(g, opt_state)
+        updates, opt_state = opt_update(gradient, opt_state)
         return optax.apply_updates(params, updates), opt_state
 
     print("Entering training loop")
 
-    test_eval_objects_loss = initiate_eval_objects_loss(
-        "test", inv_temp, alpha, beta, gamma, graph_representation, test_loader
-    )
-    train_eval_objects_loss = initiate_eval_objects_loss(
-        "train", inv_temp, alpha, beta, gamma, graph_representation, train_eval_loader
-    )
-    eval_objects_loss = test_eval_objects_loss + train_eval_objects_loss
-    test_eval_moser_loss = initiate_eval_moser_loss(
-        "test", N_STEPS_MOSER, N_RUNS_MOSER, graph_representation, test_data, sat_data
-    )
-    train_eval_moser_loss = initiate_eval_moser_loss(
-        "train",
-        N_STEPS_MOSER,
-        N_RUNS_MOSER,
+    eval_objects_loss = initiate_eval_objetcts_train_test(
+        inv_temp,
+        alpha,
+        beta,
+        gamma,
         graph_representation,
+        test_loader,
+        train_eval_loader,
+    )
+    eval_moser_loss = initiate_eval_moser_train_test(
+        n_steps_moser,
+        n_runs_moser,
+        graph_representation,
+        test_data,
         train_eval_data,
         sat_data,
     )
-    eval_moser_loss = test_eval_moser_loss + train_eval_moser_loss
+
     eval_objects_loss = update_eval_objects_loss(params, total_loss, eval_objects_loss)
-    if N_STEPS_MOSER != 0:
+    if n_steps_moser != 0:
         eval_moser_loss = update_eval_moser_loss(network, params, eval_moser_loss)
-    for epoch in range(NUM_EPOCHS):
-        print("epoch " + str(epoch + 1) + " of " + str(NUM_EPOCHS))
+    for epoch in range(num_epochs):
+        print("epoch " + str(epoch + 1) + " of " + str(num_epochs))
         start_time = time.time()
         for counter, batch in enumerate(train_loader):
             print("batch_number", counter)
@@ -229,7 +221,7 @@ def train(
         eval_objects_loss = update_eval_objects_loss(
             params, total_loss, eval_objects_loss
         )
-        if N_STEPS_MOSER != 0:
+        if n_steps_moser != 0:
             eval_moser_loss = update_eval_moser_loss(network, params, eval_moser_loss)
 
         loss_str = "Epoch {} in {:0.2f} sec".format(epoch + 1, epoch_time) + ";  "
@@ -239,23 +231,20 @@ def train(
                 + f"{eval_result.name}: {np.round(eval_result.results[-1],6)}"
                 + "; "
             )
-            if experiment_tracking == True:
+            if experiment_tracking:
                 mlflow.log_metric(eval_result.name, eval_result.results[-1], step=epoch)
-        if N_STEPS_MOSER != 0:
+        if n_steps_moser != 0:
             for eval_result in eval_moser_loss:
                 loss_str = (
                     loss_str
                     + f"{eval_result.name}: {np.round(eval_result.results[-1],4)}"
                     + "; "
                 )
-                if experiment_tracking == True:
+                if experiment_tracking:
                     mlflow.log_metric(
                         eval_result.name, eval_result.results[-1], step=epoch
                     )
         print(loss_str)
-        if epoch == 0:
-            t2 = time.time()
-            print("took", t2 - t1, "seconds")
     if model_path:
         jnp.save(
             model_path,
@@ -297,16 +286,16 @@ def train(
 
 
 def experiment_tracking_train(
-    MODEL_REGISTRY: str,
-    EXPERIMENT_NAME: str,
+    model_registry: str,
+    experiment_name: str,
     batch_size: int,
     inv_temp: float,
     alpha: float,
     beta: float,
     gamma: float,
-    NUM_EPOCHS: int,
-    N_STEPS_MOSER: int,
-    N_RUNS_MOSER: int,
+    num_epochs: int,
+    n_steps_moser: int,
+    n_runs_moser: int,
     data_path: str,
     graph_representation,
     mlp_layers: list[int],
@@ -326,9 +315,9 @@ def experiment_tracking_train(
         alpha (float): prefactor used for Gibbs loss
         beta (float): prefactor used for LLL loss
         gamma (float): prefactor used for alternative LLL loss
-        NUM_EPOCHS (int): _description_
-        N_STEPS_MOSER (int): number of steps used in MT algorithm statistics
-        N_RUNS_MOSER (int): number of runs used in MT algorithm statistics
+        num_epochs (int): _description_
+        n_steps_moser (int): number of steps used in MT algorithm statistics
+        n_runs_moser (int): number of runs used in MT algorithm statistics
         data_path (str): path pointing to training dataset (this is split into train and test set)
         graph_representation (str): Representation used. Either "LCG" or "VCG"
         mlp_layers (array): size of mlp_layers. For example: [200,200]
@@ -351,17 +340,17 @@ def experiment_tracking_train(
         network_type=network_type, graph_representation=graph_representation
     )
 
-    MODEL_REGISTRY_path = Path(MODEL_REGISTRY)
-    MODEL_REGISTRY_path.mkdir(exist_ok=True)  # create experiments dir
+    model_registry_path = Path(model_registry)
+    model_registry_path.mkdir(exist_ok=True)  # create experiments dir
 
     timestr = time.strftime("%Y%m%d-%H%M%S")
     params_save = Path("experiments", "params_save")
     params_save.mkdir(exist_ok=True)
-    model_path = os.path.join(params_save, EXPERIMENT_NAME + timestr)
+    model_path = os.path.join(params_save, experiment_name + timestr)
     img_path = model_path + "_plot"
 
-    mlflow.set_tracking_uri("file://" + str(MODEL_REGISTRY_path.absolute()))
-    mlflow.set_experiment(EXPERIMENT_NAME)
+    mlflow.set_tracking_uri("file://" + str(model_registry_path.absolute()))
+    mlflow.set_experiment(experiment_name)
     with mlflow.start_run():
         # log key hyperparameters
         mlflow.log_params(
@@ -371,9 +360,9 @@ def experiment_tracking_train(
                 "beta": beta,
                 "gamma": gamma,
                 "batch_size": batch_size,
-                "NUM_EPOCHS": NUM_EPOCHS,
-                "N_STEPS_MOSER": N_STEPS_MOSER,
-                "N_RUNS_MOSER": N_RUNS_MOSER,
+                "NUM_EPOCHS": num_epochs,
+                "N_STEPS_MOSER": n_steps_moser,
+                "N_RUNS_MOSER": n_runs_moser,
                 "network_definition": network_definition.__name__,
                 "path_dataset": data_path,
                 "graph_representation": graph_representation,
@@ -392,9 +381,9 @@ def experiment_tracking_train(
             alpha,
             beta,
             gamma,
-            NUM_EPOCHS,
-            N_STEPS_MOSER,
-            N_RUNS_MOSER,
+            num_epochs,
+            n_steps_moser,
+            n_runs_moser,
             data_path,
             mlp_layers=mlp_layers,
             img_path=img_path,
@@ -407,9 +396,9 @@ def experiment_tracking_train(
             final_learning_rate=final_learning_rate,
         )
         # log params which are a result of learning
-        with tempfile.TemporaryDirectory() as dp:
-            joblib.dump(artifacts["params"], Path(dp, "params.pkl"))
-            mlflow.log_artifact(dp)
+        with tempfile.TemporaryDirectory() as dump_object:
+            joblib.dump(artifacts["params"], Path(dump_object, "params.pkl"))
+            mlflow.log_artifact(dump_object)
 
 
 if __name__ == "__main__":
--- python/src/train_utils.py
+++ python/src/train_utils.py
@@ -15,7 +15,7 @@ EvalResults = collections.namedtuple(
 
 def initiate_eval_objects_loss(
     text: str,
-    f: float,
+    inv_temp: float,
     alpha: float,
     beta: float,
     gamma: float,
@@ -26,7 +26,7 @@ def initiate_eval_objects_loss(
 
     Args:
         text (str): description of loss type as str (typically "test" and "train")
-        f (float): inverse temperature
+        inv_temp (float): inverse temperature
         alpha (float): used pre-factor for the Gibbs loss
         beta (float): used pre-factor for the LLL loss
         gamma (float): used pre-factor for the alternative LLL loss
@@ -37,14 +37,16 @@ def initiate_eval_objects_loss(
         EvalResults: initialised EvalResults object for loss functions
     """
     eval_total = EvalResults(
-        text + " total loss", [], False, [f, alpha, beta, gamma], rep, loader
+        text + " total loss", [], False, [inv_temp, alpha, beta, gamma], rep, loader
     )
     eval_dm = EvalResults(
-        text + " loss Gibbs", [], False, [f, alpha, 0, 0], rep, loader
+        text + " loss Gibbs", [], False, [inv_temp, alpha, 0, 0], rep, loader
     )
-    eval_lll = EvalResults(text + " loss LLL", [], False, [f, 0, beta, 0], rep, loader)
+    eval_lll = EvalResults(
+        text + " loss LLL", [], False, [inv_temp, 0, beta, 0], rep, loader
+    )
     eval_alt_lll = EvalResults(
-        text + " loss alt_LLL", [], False, [f, 0, 0, gamma], rep, loader
+        text + " loss alt_LLL", [], False, [inv_temp, 0, 0, gamma], rep, loader
     )
 
     eval_objects_loss = [
@@ -60,8 +62,8 @@ def initiate_eval_objects_loss(
 
 def initiate_eval_moser_loss(
     text: str,
-    N_STEPS_MOSER: float,
-    N_RUNS_MOSER: float,
+    n_steps_moser: float,
+    n_runs_moser: float,
     rep: SATRepresentation,
     data_subset,
     sat_data,
@@ -70,8 +72,8 @@ def initiate_eval_moser_loss(
 
     Args:
         text (str): description of loss type as str (typically "test" and "train")
-        N_STEPS_MOSER (float): number of steps executed by MT algorithm for the loss
-        N_RUNS_MOSER (float): number of runs executed by MT algorithm for the loss
+        n_steps_moser (float): number of steps executed by MT algorithm for the loss
+        n_runs_moser (float): number of runs executed by MT algorithm for the loss
         rep (SATRepresentation): SATRepresentation chosen here
         data_subset (@TODO: specify type): subset of dataset we want to look at
         sat_data (@TODO: specify type): full sat_data
@@ -83,7 +85,7 @@ def initiate_eval_moser_loss(
         text + " loss model Moser",
         [],
         False,
-        [N_STEPS_MOSER, N_RUNS_MOSER, "model"],
+        [n_steps_moser, n_runs_moser, "model"],
         rep,
         [data_subset, sat_data],
     )
@@ -91,7 +93,7 @@ def initiate_eval_moser_loss(
         text + " loss uniform Moser",
         [],
         False,
-        [N_STEPS_MOSER, N_RUNS_MOSER, "uniform"],
+        [n_steps_moser, n_runs_moser, "uniform"],
         rep,
         [data_subset, sat_data],
     )
@@ -112,23 +114,50 @@ def update_eval_objects_loss(params, loss, eval_objects_loss):
     Returns:
         EvalResults: updated EvalResults loss function object
     """
-    for i in range(len(eval_objects_loss)):
-        eval_objects_loss[i].results.append(
+    for _, eval_object in enumerate(eval_objects_loss):
+        eval_object.results.append(
             np.mean(
                 [
                     loss(
                         params,
                         b,
-                        eval_objects_loss[i].loss_params[0],
-                        eval_objects_loss[i].loss_params[1],
-                        eval_objects_loss[i].loss_params[2],
-                        eval_objects_loss[i].loss_params[3],
-                        eval_objects_loss[i].rep,
+                        eval_object.loss_params[0],
+                        eval_object.loss_params[1],
+                        eval_object.loss_params[2],
+                        eval_object.loss_params[3],
+                        eval_object.rep,
                     )
-                    for b in eval_objects_loss[i].loader
+                    for b in eval_object.loader
                 ]
             )
         )
+    return eval_objects_loss
+
+
+def initiate_eval_objetcts_train_test(
+    inv_temp, alpha, beta, gamma, graph_representation, test_loader, train_loader
+):
+    """Initiate eval_objects both for train and test data.
+
+    Args:
+        inv_temp (float): inverse temperature used in Gibbs Loss
+        alpha (float): prefactor used for Gibbs loss
+        beta (float): prefactor used for LLL loss
+        gamma (float): prefactor used for alternative LLL loss
+        graph_representation (SATRepresentation): SATRepresentation used. Either LCG or VCG. Note: here you have to use the SATRepresentation!!!
+        test_loader (@TODO: specify type): JraphDataLoader object of test_data
+        train_loader (@TODO: specify type): JraphDataLoader object of train_data
+
+    Returns:
+        eval_objects both for train and test data
+    """
+    test_eval_objects_loss = initiate_eval_objects_loss(
+        "test", inv_temp, alpha, beta, gamma, graph_representation, test_loader
+    )
+    train_eval_objects_loss = initiate_eval_objects_loss(
+        "train", inv_temp, alpha, beta, gamma, graph_representation, train_loader
+    )
+    eval_objects_loss = test_eval_objects_loss + train_eval_objects_loss
     return eval_objects_loss
 
 
@@ -143,22 +172,19 @@ def update_eval_moser_loss(network, params, eval_moser_loss):
     Returns:
         EvalResults: updated EvalResults Moser loss object
     """
-    for i in range(len(eval_moser_loss)):
-        if (
-            eval_moser_loss[i].loss_params[2] == "model"
-            or len(eval_moser_loss[i].results) == 0
-        ):
-            eval_moser_loss[i].results.append(
+    for i, eval_moser in enumerate(eval_moser_loss):
+        if eval_moser.loss_params[2] == "model" or len(eval_moser.results) == 0:
+            eval_moser.results.append(
                 evaluate_moser_rust(
-                    eval_moser_loss[i].loader[1],
+                    eval_moser.loader[1],
                     network,
                     params,
-                    eval_moser_loss[i].loader[0],
-                    eval_moser_loss[i].rep,
-                    mode_probabilities=eval_moser_loss[i].loss_params[2],
-                    N_STEPS_MOSER=eval_moser_loss[i].loss_params[0],
-                    N_RUNS_MOSER=eval_moser_loss[i].loss_params[1],
-                    SEED=0,
+                    eval_moser.loader[0],
+                    eval_moser.rep,
+                    mode_probabilities=eval_moser.loss_params[2],
+                    n_steps_moser=eval_moser.loss_params[0],
+                    n_runs_moser=eval_moser.loss_params[1],
+                    seed=0,
                 )[0]
             )
         elif eval_moser_loss[i].loss_params[2] == "uniform":
@@ -166,9 +192,40 @@ def update_eval_moser_loss(network, params, eval_moser_loss):
     return eval_moser_loss
 
 
+def initiate_eval_moser_train_test(
+    n_steps_moser, n_runs_moser, graph_representation, test_data, train_data, sat_data
+):
+    """Initiate eval_moser both for train and test data.
+
+    Args:
+        n_steps_moser (int): number of steps used in MT algorithm statistics
+        n_runs_moser (int): number of runs used in MT algorithm statistics
+        graph_representation (SATRepresentation): SATRepresentation used. Either LCG or VCG. Note: here you have to use the SATRepresentation!!!
+        test_data (@TODO: specify type): test_data = subset of sat_data used for testing
+        train_data (@TODO: specify type): train_data = subset of sat_data used for training
+        sat_data (@TODO: specify type): sat dataset
+
+    Returns:
+        eval_moser object both for training and testing
+    """
+    test_eval_moser_loss = initiate_eval_moser_loss(
+        "test", n_steps_moser, n_runs_moser, graph_representation, test_data, sat_data
+    )
+    train_eval_moser_loss = initiate_eval_moser_loss(
+        "train",
+        n_steps_moser,
+        n_runs_moser,
+        graph_representation,
+        train_data,
+        sat_data,
+    )
+    eval_moser_loss = test_eval_moser_loss + train_eval_moser_loss
+    return eval_moser_loss
+
+
 def plot_accuracy_fig(*eval_results):
     """Do a plot containing the losses as a function of the epochs."""
-    ROLLING_WINDOW_SIZE = 1
+    rolling_window_size = 1
     for eval_result in eval_results:
         results = np.array(eval_result.results)
         if eval_result.normalize:
@@ -176,7 +233,7 @@ def plot_accuracy_fig(*eval_results):
         plt.plot(
             # np.arange(0, NUM_EPOCHS - ROLLING_WINDOW_SIZE, 1),
             pd.Series(results)
-            .rolling(np.min([ROLLING_WINDOW_SIZE, len(results)]))
+            .rolling(np.min([rolling_window_size, len(results)]))
             .mean(),
             "o--",
             label=eval_result.name,
@@ -192,9 +249,9 @@ def evaluate_moser_rust(
     data_subset,
     representation: SATRepresentation,
     mode_probabilities="model",
-    N_STEPS_MOSER=100,
-    N_RUNS_MOSER=1,
-    SEED=0,
+    n_steps_moser=100,
+    n_runs_moser=1,
+    seed=0,
 ):
     """Run MT algorithm in rust and get results. This is used above.
 
@@ -205,9 +262,9 @@ def evaluate_moser_rust(
         data_subset (@TODO: specify type): data_subset used for evaluation
         representation (SATRepresentation): SATRepresentation used here
         mode_probabilities (str, optional): either "model" or "uniform" -> "model" means we use the model for the oracle and "uniform" means we use a uniform oracle in the MT algorithm. Defaults to "model".
-        N_STEPS_MOSER (int, optional): number of steps MT algorithm takes. Defaults to 100.
-        N_RUNS_MOSER (int, optional): number of runs MT algorithm takes. Defaults to 1.
-        SEED (int, optional): SEED used in MT algorithm. Defaults to 0.
+        n_steps_moser (int, optional): number of steps MT algorithm takes. Defaults to 100.
+        n_runs_moser (int, optional): number of runs MT algorithm takes. Defaults to 1.
+        seed (int, optional): SEED used in MT algorithm. Defaults to 0.
 
     Returns:
         (@TODO:specify type): (np.mean(av_energies), np.mean(av_entropies)) -> mean energies = #violated clauses / #clause after N_STEPS_MOSER and mean entropy of the oracle probabilities
@@ -218,13 +275,13 @@ def evaluate_moser_rust(
     for idx in data_subset.indices:
         problem_path = sat_data.instances[idx].name + ".cnf"
         problem = sat_data.get_unpadded_problem(idx)
-        n, _, _ = problem.params
+        n_variables, n_clauses, _ = problem.params
         if mode_probabilities == "uniform":
-            model_probabilities = np.ones(n) / 2
+            model_probabilities = np.ones(n_variables) / 2
         elif mode_probabilities == "model":
             decoded_nodes = network.apply(params, problem.graph)
             model_probabilities = representation.get_model_probabilities(
-                decoded_nodes, n
+                decoded_nodes, n_variables
             )
         else:
             print("not valid argument for mode_probabilities")
@@ -240,13 +297,12 @@ def evaluate_moser_rust(
             "moser",
             problem_path,
             model_probabilities,
-            N_STEPS_MOSER,
-            N_RUNS_MOSER,
-            SEED,
+            n_steps_moser,
+            n_runs_moser,
+            seed,
             return_trajectories=False,
         )
-        _, m, _ = problem.params
-        av_energies.append(np.mean(final_energies) / m)
+        av_energies.append(np.mean(final_energies) / n_clauses)
         prob = np.vstack(
             (
                 np.ones(model_probabilities.shape[0]) - model_probabilities,
